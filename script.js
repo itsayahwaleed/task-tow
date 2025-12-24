6a41252f1fc86a3da342adc25e312db2685697d8
ayahwaleed <ayahwaleedra@icloud.com>
2025-12-24 18:12:39 +0200

    add elements

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 let tasks = JSON.parse(localStorage.getItem('js_tasks')) || [];
 let currentFilter = 'all';
 let currentEditId = null;
+// 2. العناصر المستهدفة
+const taskInput = document.querySelector('#taskInput');
+const addTaskBtn = document.querySelector('#addTaskBtn');
+const taskList = document.querySelector('#taskList');
+const errorMessage = document.querySelector('#errorMessage');
+const customModal = document.querySelector('#customModal');
+const modalInput = document.querySelector('#modalInput');
+const modalError = document.querySelector('#modalError');
